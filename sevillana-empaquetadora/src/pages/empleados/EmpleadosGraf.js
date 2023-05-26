2c1f01937c31103d8fcd55ec730cf94432c5064a
Daniel <silverhawkdeveloper@gmail.com>
2023-05-26 21:24:18 +0200

    grafica

--- sevillana-empaquetadora/src/pages/empleados/EmpleadosGraf.js
+++ sevillana-empaquetadora/src/pages/empleados/EmpleadosGraf.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { Link, useNavigate } from "react-router-dom";
 import logo from '../../imagenes/logo.png';
+import { Chart } from 'chart.js/auto';
 
 import '../../css/app.css';
 import '../../css/home.css';
@@ -9,7 +10,7 @@ import '../../css/home.css';
 const EmpleadosGraf = () => {
   const navigate = useNavigate();
 
-  useEffect(() => {
+  useEffect( () => {
     // La función que deseas ejecutar al montar el componente
     const usuario = document.getElementById('usuario');
     const url = 'http://localhost:5000/auth-token/profile';
@@ -18,7 +19,63 @@ const EmpleadosGraf = () => {
     const token = sessionStorage.getItem('JWT');
     auth_token_profile(url, token, usuario);
 
+    mostrar_grafica();
+
   });
+
+  async function mostrar_grafica() {
+        // Recuperamos los pedidos
+        const pedidos = await peticionPedidos();
+        let array_pedidos = [];
+    
+        // Recuperamos los usuarios
+        const usuarios = await peticionUsuarios();
+        let array_usuario = [];
+    
+        let contador = 0;
+        usuarios.forEach(us => {
+          array_usuario.push(us.nombre);
+          pedidos.forEach(ped => {
+            if (us._id === ped.usuario[0]._id) {
+              contador++;
+            }
+          })
+          array_pedidos.push(contador);
+          contador = 0;
+        });
+    
+        const ctx = document.getElementById('myChart');
+        new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels: array_usuario,
+            datasets: [{
+              label: 'Número de pedidos',
+              data: array_pedidos,
+              borderWidth: 1
+            }]
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true
+              }
+            }
+          }
+        });
+  }
+
+  async function peticionPedidos() {
+    const response = await fetch('http://localhost:5000/pedido/');
+    const data = await response.json();
+    return data;
+  }
+
+  async function peticionUsuarios() {
+    const response = await fetch('http://localhost:5000/usuario/');
+    const data = await response.json();
+    return data;
+  }
 
   function auth_token_profile(url, token, usuario) {
     fetch(url, {
@@ -45,6 +102,12 @@ const EmpleadosGraf = () => {
     navigate('/');
   }
 
+  const estilo = {
+    'display': 'flex',
+    'justify-content': 'center',
+    'align-content': 'center'
+  };
+
   return (
     <div id="cntr">
 
@@ -60,7 +123,11 @@ const EmpleadosGraf = () => {
       </div>
 
       <div id="cntr_blanco">
-
+        <div id='cntr_grafica'>
+          <div id='grafica'>
+            <canvas id="myChart" ></canvas>
+          </div>
+        </div>
       </div>
 
     </div>
