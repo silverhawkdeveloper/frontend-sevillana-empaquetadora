6d3ac7125374118587983d54460395262cd6286a
Daniel <silverhawkdeveloper@gmail.com>
2023-05-27 22:12:09 +0200

    orden tabla

--- sevillana-empaquetadora/src/pages/pedidos/Pedidos.js
+++ sevillana-empaquetadora/src/pages/pedidos/Pedidos.js
@@ -78,6 +78,66 @@ const Pedidos = () => {
       })
   }
 
+  function ordenarTabla(index) {
+    let table, rows, bandera, i, x, y, n, cambiar, dir, contador = 0;
+    table = document.getElementById("tabla_pedidos");
+    bandera = true;
+
+    switch (index.currentTarget.textContent) {
+      case 'Fecha':
+        n = 0;
+        break;
+      case 'Caja':
+        n = 1;
+        break;
+      default:
+        break;
+    }
+
+    // Dirección de clasificación ascendente
+    dir = "asc";
+
+    while (bandera) {
+      // No se realiza ningún cambio
+      bandera = false;
+      rows = table.rows;
+      // Recorre todas las filas de la tabla excepto el encabezado
+      for (i = 1; i < (rows.length - 1); i++) {
+        // Comience diciendo que no debería haber cambios
+        cambiar = false;
+        // Los dos elementos para comparar, uno de la fila actual y uno de la siguiente
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 1].getElementsByTagName("TD")[n];
+        if (dir == "asc") {
+          // Comprobamos las dos filas
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            // Cambiamos el orden
+            cambiar = true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            // Cambiamos el orden
+            cambiar = true;
+            break;
+          }
+        }
+      }
+      if (cambiar) {
+        // Hacemos el cambio y activamos la bandera
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        bandera = true;
+        contador++;
+      } else {
+        // Cambiamos la dirección
+        if (contador == 0 && dir == "asc") {
+          dir = "desc";
+          bandera = true;
+        }
+      }
+    }
+  }
+
   return (
     <div id='cntr'>
       <div id="cntr_negro">
@@ -112,12 +172,12 @@ const Pedidos = () => {
           </div>
 
           <div id="cntr_gris_tabla">
-            <table>
+            <table id='tabla_pedidos'>
               <tbody id='tbody'>
                 <tr>
-                  <th className='cabecera'>Fecha<img className='down' src={down} alt='down'></img></th>
+                  <th className='cabecera' onClick={ordenarTabla}>Fecha<img className='down' src={down} alt='down'></img></th>
                   <th>Producto</th>
-                  <th className='cabecera'>Caja<img className='down' src={down} alt='down'></img></th>
+                  <th className='cabecera' onClick={ordenarTabla}>Caja<img className='down' src={down} alt='down'></img></th>
                   <th>Merma cm³</th>
                   <th>Realizado</th>
                   <th>Modificar</th>
